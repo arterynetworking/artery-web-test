0bebe54f245cd3a0e021c2fa78ad3c92117248f5
Macklin Hartley <macklin@outlook.com>
2016-01-21 22:08:17 +0800

    Logical stopping point

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 /* global angular */
+
 angular.module("arteryWebApp", [])
     .controller("MainController", ["$scope", function ($scope) {
         $scope.templateUrl = "pages/home.html";
+    }])
+    .controller("HomeController", ["$scope", function ($scope) {
+        
     }]);
