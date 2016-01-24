d54658cbf132a0b944d7ae86c079f0b88496edc5
Macklin Hartley <macklin@outlook.com>
2016-01-24 18:46:59 +0800

    commit.

--- js/controllers/global.js
+++ js/controllers/global.js
@@ -1,2 +1,27 @@
 /* global angular */
 var app = angular.module("arteryWebApp", []);
+
+app.directive("mapscroll", function ($document) {
+    return function(scope, element, attrs) {
+        
+        /* Enable map scroll upon click of maps. */
+        element.on("click", function () {
+           element.find("iframe").addClass("enable-pointer");
+        });
+        
+        /* Disable map scroll when mouse leaves maps. */
+        element.on("mouseleave", function () {
+           element.find("iframe").removeClass("enable-pointer");
+        });
+    }
+});
+// 
+// $(document).ready(function() {
+//     $('.maps').click(function () {
+//         $('.maps iframe').css("pointer-events", "auto");
+//     });
+//     
+//     $( ".maps" ).mouseleave(function() {
+//       $('.maps iframe').css("pointer-events", "none"); 
+//     });
+//  });
