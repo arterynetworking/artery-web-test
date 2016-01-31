6bc53798ee78d8610e6ccb33a6fc08f0cecab7cb
Macklin Hartley <macklin@outlook.com>
2016-01-31 14:34:08 +0800

    latest bits

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,25 +1,57 @@
 /* main controller */
-app.controller("MainController", ["$scope", "$window", "$location",
-    function ($scope, $window, $location) {
+app.controller("MainController", ["$scope", "$window", "$location", "smoothScroll",
+    function ($scope, $window, $location, smoothScroll) {
 
         $scope.defaultPage = "pages/home.html"
         $scope.templateUrl = null;
-        $scope.isLoading = false;
+        $scope.hideNav = true;
 
         $scope.goToPage = function (pageName) {
-            
+
             $location.url(pageName);
             $scope.templateUrl = "pages" + pageName + ".html";
+            
+            $scope.goToTop();
+
+            $scope.scrollPageEvent();
         }
         
+        $scope.goToTop = function () {
+            
+            var element = document.getElementById("top");
+            smoothScroll(element);
+        }
+
+        $scope.scrollPageEvent = function (applyScope) {
+
+            if ($scope.templateUrl !== $scope.defaultPage) {
+                $scope.hideNav = false;
+            } else {
+                if ($window.pageYOffset >= $window.innerHeight * 0.4) {
+                    $scope.hideNav = false;
+                } else {
+                    $scope.hideNav = true;
+                }
+            }
+
+            if (applyScope === true) {
+                $scope.$apply();
+            }
+        }
+
         $scope.loadPageEvent = function () {
 
             if ($location.url() !== "") {
                 $scope.goToPage($location.url());
             } else {
                 $scope.templateUrl = $scope.defaultPage;
-                $scope.$apply();
             }
+            
+            $scope.$apply();
+
+            angular.element($window).bind("scroll", function () {
+                $scope.scrollPageEvent(true)
+            });
         }
 
         angular.element($window).bind("load", $scope.loadPageEvent);
